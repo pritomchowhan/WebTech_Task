93c4fe7235b4bf1b5b4fd3989d521a9b66438878
PRITOM CHOWHAN <136151371+pritomchowhan@users.noreply.github.com>
2023-08-19 01:16:25 +0600

    Delete LAb Task 5 directory

--- LAb Task 5/Lab code/nav.php
+++
@@ -1,5 +0,0 @@
- <ul>
-        <li><a href="addProduct.php"> Add product</a></li>
-        <li><a href="showAllProducts.php"> Show all products</a></li>
-        <li><a href="searchProduct.php"> Search products</a></li>
-    </ul>
